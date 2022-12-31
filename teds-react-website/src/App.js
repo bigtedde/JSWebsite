c4883bb035b10205fb6fa0c9d2546c277a5edf4e
bigtedde <big.tedde@gmail.com>
2022-12-31 12:58:06 -0800

    added links, corrected formatting

--- teds-react-website/src/App.js
+++ teds-react-website/src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.jpg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to my website!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Ted's Place!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </header>
+        <body className='App-body'>
+          <p>List of links to my various projects</p>
+          <ul className='App-list'>
+              <li><a href="https://github.com/bigtedde/leetCode">Leetcode Examples</a></li>
+              <li><a href="https://github.com/bigtedde/TickerChecker">Ticker Checker (iOS App)</a></li>
+              <li><a href="https://github.com/bigtedde/Gambling-Simulator">Gambling Simulator (iOS App)</a></li>
+              <li><a href="https://github.com/bigtedde/react">Practicing with React</a></li>
+              <li><a href="https://github.com/bigtedde/TedsWebsite">My HTML Website</a></li>
+              <li><a href="https://github.com/bigtedde/JSWebsite">My ReactJS Website</a></li>
+          </ul>
+        </body>
     </div>
   );
-}
 
+}
 export default App;
