24cc5e9b247ffac0f2e637657caeb32f55467afd
bigtedde <big.tedde@gmail.com>
2023-02-12 19:41:50 -0800

    updated logo and new projects

--- teds-react-website/src/App.js
+++ teds-react-website/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.JPG';
+import logo from './logo.jpg';
 import './App.css';
 
 
@@ -7,12 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to my website!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         </header>
         <body className='App-body'>
           <p>List of links to my various projects</p>
           <ul className='App-list'>
-              <li><a href='https://github.com/bigtedde/Tic-Tac-Toe-with-AI'>Tic-Tac-Toe w/Unbeatable AI </a></li>
+              <li><a href='https://github.com/bigtedde/Tic-Tac-Toe-with-AI'>Tic-Tac-Toe w/Unbeatable AI</a></li>
               <li><a href="https://github.com/bigtedde/leetCode">Leetcode Examples</a></li>
               <li><a href="https://github.com/bigtedde/TickerChecker">Ticker Checker (iOS App)</a></li>
               <li><a href="https://github.com/bigtedde/Gambling-Simulator">Gambling Simulator (iOS App)</a></li>
